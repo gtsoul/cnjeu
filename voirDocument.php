d679fef77777da9da69c4365121992be6abb18ab
Arnaud Demaiziere <gtsoul@gmail.com>
2014-09-04 22:33:51 +0200

    Maj css listes autocomplètes + secu détail

--- voirDocument.php
+++ voirDocument.php
@@ -1,15 +1,36 @@
-<?
-$link = mysql_connect("mysql51-41.pro","cnjeucnjjeux","psz5qAdfs") or die("Impossible de se connecter : " . mysql_error());
+<?php
 
-mysql_select_db('cnjeucnjjeux');
+define( '_JEXEC', 1 );
 
-// on crée la requête SQL
-$sql = "SELECT path_hd,path_miniature,path_optimise FROM cnj_document WHERE id = ".$_GET['id'];
+define('JPATH_BASE', dirname(__FILE__) );
 
-// on envoie la requête
-$req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
+define( 'DS', DIRECTORY_SEPARATOR );
 
-$data = mysql_fetch_assoc($req);
+require_once ( JPATH_BASE .DS.'includes'.DS.'defines.php' );
+require_once ( JPATH_BASE .DS.'includes'.DS.'framework.php' );
+
+$sql = "SELECT path_hd,path_miniature,path_optimise FROM cnj_document WHERE id = ".htmlentities($_GET['id']);
+
+$db	= JFactory::getDBO();
+$db->setQuery($sql);
+$data = $db->loadObjectList();
+
+function endsWith($haystack, $needle)
+{
+    return $needle === "" || substr($haystack, -strlen($needle)) === $needle;
+}
+function displayDocument($object, $path) {
+  if($object->$path != '') {
+    if(endsWith($object->$path, '.pdf') || endsWith($object->$path, '.PDF')) {
+      echo '<td><a href="'.$object->$path.'">'.$object->$path.'</a></td>';
+    } else {
+      echo '<td><a href="'.$object->$path.'"><img src="'.$object->$path.'" style="max-width: 200px;"></a></td>';
+    }
+  } else {
+    echo('<td></td>');
+  }
+}
+
 echo '<input type="button" value="fermer" onclick="self.close()"><br/>';	
 echo '<table>';
 echo '<tr>';
@@ -18,16 +39,17 @@ echo '<tr>';
     echo '<td>miniature : </td>';
     echo '<td>vignette : </td>';
     echo '<td>haute d&eacute;finition : </td>';
- 
-echo "</tr><tr>";
-    echo '<td><a href="'.$data['path_miniature'].'""><img src="'.$data['path_miniature'].'" style="max-width: 200px;"></a></td>';
-    echo '<td><a href="'.$data['path_optimise'].'""><img src="'.$data['path_optimise'].'" style="max-width: 250px;" ></a></td>';
-    echo '<td><a href="'.$data['path_hd'].'""><img src="'.$data['path_hd'].'" style="max-width: 300px;"></a></td>';
+
+for ($i = 0; $i < count($data); $i++) {
+    $d = $data[$i];
+    echo "</tr><tr>";
+    displayDocument($d, 'path_miniature');    
+    displayDocument($d, 'path_optimise');    
+    displayDocument($d, 'path_hd');
+}   
 echo '</tr>';
 echo '</table>';
 
 echo '<input type="button" value="fermer" onclick="self.close()">';	
-
-mysql_close($link); 
 
 ?>
